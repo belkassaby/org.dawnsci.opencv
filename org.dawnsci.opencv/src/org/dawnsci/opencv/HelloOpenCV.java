0917e8bf0e04f6af6f440533fd424fb25aec3a34
Baha El Kassaby <baha.elkassaby@gmail.com>
2016-12-21 16:20:37 +0000

    1rst commit

---
+++ org.dawnsci.opencv/src/org/dawnsci/opencv/HelloOpenCV.java
@@ -0,0 +1,82 @@
+package org.dawnsci.opencv;
+
+import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
+import org.eclipse.dawnsci.analysis.api.io.IDataHolder;
+import org.eclipse.dawnsci.analysis.dataset.impl.RGBDataset;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.highgui.Highgui;
+import org.opencv.objdetect.CascadeClassifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.ac.diamond.scisoft.analysis.io.LoaderFactory;
+
+//
+// Detects faces in an image, draws boxes around them, and writes the results
+// to "faceDetection.png".
+//
+class DetectFaceDemo {
+	public void run() {
+		System.out.println("\nRunning DetectFaceDemo");
+		HelloOpenCV.logger.debug("Running DetectFace");
+		IDataset data = null;
+		//loading IDataset
+		IDataHolder holder;
+		try {
+			holder = LoaderFactory.getData("/scratch/workspace/DAWN_git/dawn-common.git/org.dawnsci.boofcv/resources/lena.png");
+			data = holder.getDataset(0);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		int[] shape = data.getShape();
+
+		// create openCV matrix
+		Mat image = new Mat(shape[0], shape[1], CvType.CV_16S);
+		image.put(0, 0, ((RGBDataset)data).getData());
+		
+		
+		// Create a face detector from the cascade file in the resources
+		// directory.
+		CascadeClassifier faceDetector = new CascadeClassifier("/resources/lbpcascade_frontalface.xml");
+//		Mat image = Highgui.imread("/resources/lena.png");
+		
+		
+		// Detect faces in the image.
+		// MatOfRect is a special container class for Rect.
+		MatOfRect faceDetections = new MatOfRect();
+		faceDetector.detectMultiScale(image, faceDetections);
+
+		System.out.println(String.format("Detected %s faces", faceDetections.toArray().length));
+
+		// Draw a bounding box around each face.
+		for (Rect rect : faceDetections.toArray()) {
+			Core.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
+					new Scalar(0, 255, 0));
+		}
+
+		// Save the visualized detection.
+		String filename = "faceDetection.png";
+		System.out.println(String.format("Writing %s", filename));
+		Highgui.imwrite(filename, image);
+	}
+}
+
+public class HelloOpenCV {
+	public static Logger logger = LoggerFactory.getLogger(HelloOpenCV.class);
+	public static void main(String[] args) {
+		System.out.println("Hello, OpenCV");
+
+		// Load the native library.
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		new DetectFaceDemo().run();
+	}
+}
---
+++ org.dawnsci.opencv/src/org/dawnsci/opencv/OpenCVTest.java
@@ -0,0 +1,14 @@
+package org.dawnsci.opencv;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+
+public class OpenCVTest {
+
+	public static void main(String[] args) {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME );
+		Mat m = Mat.eye(3, 3, CvType.CV_8UC1);
+		System.out.println("m = " + m.dump());
+	}
+}
